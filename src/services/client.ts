d0629503c84662f7588eca51ed562485d536a601
Caio Batista <caiobatis7@gmail.com>
2023-01-30 14:41:43 -0300

    fix: fix env var setup

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,9 @@
-import { API_BASE_URL } from '@env'
 import axios from 'axios'
 
 import * as interceptors from './interceptors'
 
 const client = axios.create({
-  baseURL: API_BASE_URL ?? 'https://5b67c708f793e00014562619.mockapi.io/api', // for security
+  baseURL: process.env.API_BASE_URL ?? 'https://5b67c708f793e00014562619.mockapi.io/api', // for security
   timeout: 8000,
   headers: {
     'Content-Type': 'application/json'
